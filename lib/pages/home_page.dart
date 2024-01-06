24588db4e7a6f09ff15cf366d84bbf2238e9bb98
Prathamesh Rokade <prathameshr955@gmail.com>
2024-01-06 18:20:33 +0530

    Completed Design of Drop Down Widget

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -52,21 +52,45 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _destinationDropDownWidget() {
-    List<DropdownMenuItem<String>> _items = [
+    List<String> _items = [
       "Indian Web Station",
       "NASA Web Station",
-    ].map(
-      (e) {
-        return DropdownMenuItem(
-          value: e,
-          child: Text(e),
-        );
-      },
-    ).toList();
+    ];
     return Container(
+      padding: EdgeInsets.symmetric(horizontal: _deviceWidth * 0.05),
+      width: _deviceWidth,
+      decoration: BoxDecoration(
+        color: const Color.fromRGBO(
+          53,
+          53,
+          53,
+          1.0,
+        ),
+        borderRadius: BorderRadius.circular(
+          10,
+        ),
+      ),
       child: DropdownButton(
+        value: _items.first,
         onChanged: (_) {},
-        items: _items,
+        items: _items.map(
+          (e) {
+            return DropdownMenuItem(
+              value: e,
+              child: Text(e),
+            );
+          },
+        ).toList(),
+        underline: Container(),
+        dropdownColor: const Color.fromRGBO(
+          53,
+          53,
+          53,
+          1.0,
+        ),
+        style: const TextStyle(
+          color: Colors.white,
+        ),
       ),
     );
   }
